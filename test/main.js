1a210b488dadfe605895d02434f58c3e894dad17
axion014 <axion014@gmail.com>
2017-09-03 17:14:30 +0900

    add blank test

---
+++ test/main.js
@@ -0,0 +1,13 @@
+phina.globalize();
+
+phina.define('MainScene', {
+	superClass: 'Scene',
+	init: function() {
+		this.superInit();
+	}
+});
+
+phina.main(function() {
+	var app = ThreeApp({});
+	app.replaceScene(MainScene());
+});
