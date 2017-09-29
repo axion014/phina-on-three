fd35e0a5b15ddd66388e1827b3380604f50f5d2e
axion014 <axion014@gmail.com>
2017-09-29 12:40:55 +0900

    bug fix/build

--- src/inputpatch.js
+++ src/inputpatch.js
@@ -10,6 +10,6 @@ phina.namespace(function() {
   phina.input.Input.prototype._move = function(x, y) {
     var elm = this.domElement;
     this._tempPosition.x = x * this.width / (elm.style.width ? parseInt(elm.style.width) : elm.width);
-    this._tempPosition.x = y * this.height / (elm.style.height ? parseInt(elm.style.height) :   elm.height);
+    this._tempPosition.y = y * this.height / (elm.style.height ? parseInt(elm.style.height) :   elm.height);
   };
 });
